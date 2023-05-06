12fb4b6f90fefe14820a19587877492d389b0ba1
Ai-Mei <purcia.princessmae296@gmail.com>
2023-05-06 14:57:58 +0800

    Initial pseudocode, without the try-catch method.

--- SIMPLE-APP-CALCULATOR.py
+++ SIMPLE-APP-CALCULATOR.py
@@ -1,1 +1,14 @@
+# Ask for user's 2 input numbers.
+# Ask for user's input for operation.
+
+# If the user's input for operation is +, add the 2 input numbers.
+# If the user's input for operation is -, subtract the second number from the first number.
+# If the user's input for operation is *, multiply the first and second input numbers.
+# If the user's input for operation is /, divide the first input number by the second input number.
+
+# Print the answer.
+# Ask the user if they need to use the calculator again.
+# If yes, repeat the process.
+# Else, if no, end the program.
+
 
